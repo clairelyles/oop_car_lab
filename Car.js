1ea42edbe558f182bd5d23c8cb99d433b0c82dbb
clairelyles <claire.lyles@gmail.com>
2014-11-25 17:04:13 -0800

    completed phase 1 - 3 of OOP lab

--- Car.js
+++ Car.js
@@ -1,25 +1,71 @@
-function Car(make, model, year, color, seats){
+// THE constructor
+function Car(make, model, year, color, seats, passengers){
   this.make = make;
   this.model = model;
-  //TODO: add year, color, seats here
-
+  this.year = year;
+  this.color = color;
+  this.seats = seats;
   this.running = false;
   this.owner = "manufacturer";
   this.previousOwners = [];
+  this.passengers = (passengers) ? passengers : [];
 }
 
 Car.prototype.sell = function(newOwner){
-
+	this.previousOwners.push(this.owner);
+	this.owner = newOwner;
 };
 
 Car.prototype.paint = function(newColor){
-
+	this.color = newColor;
 };
 
+Car.prototype.start = function(newValue) {
+	this.running = true;
+}
 
+Car.prototype.off = function(newValye) {
+	this.running = false;
+}
 
+Car.prototype.driveTo = function(destination) {
+	if (this.running === true) {
+	console.log("driving to " + destination);
+	return true;
+	}
+	return false;
+}
 
+Car.prototype.park = function(parked) {
+	if (this.running === false) {
+		console.log("parked!!");
+		return true
+	}
+	return false
+}
 
+/* ----- Phase 3 ----- */
+Car.prototype.pickUp = function(name) {
+	if (this.running === true && this.passengers.length < this.seats-1) {
+		console.log("driving to pick up " + name);
+		this.passengers.push(name);
+	return true
+	}
+return false
+}
+
+Car.prototype.dropOff = function(name) {
+	index = this.passengers.indexOf(name);
+	if (index != -1 && this.running) {
+		this.passengers.splice(index, 1);
+		return true;
+	}
+	return false;
+}
+
+Car.prototype.passengerCount = function() {
+	return this.passengers.length;
+}
 
 
 
